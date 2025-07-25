19c2f9d227d145b90277d50a1343d82c04c6eaf3
Rodrigo Centellas <101054195+Rodrigo-Centellas@users.noreply.github.com>
2025-07-25 12:48:11 -0400

    atest

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,18 @@ import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
     base: '/inf513/grupo20sa/proyecto2/renta_car_web2/public/',
+    build: {
+        manifest: true,
+        outDir: 'public/build',
+        rollupOptions: {
+            input: 'resources/js/app.js'
+        }
+    },
     plugins: [
         laravel({
             input: 'resources/js/app.js',
             refresh: true,
+            publicDirectory: 'public',
         }),
         vue({
             template: {
