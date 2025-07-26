ebf7c09813614efa0bc2f62654b5ab53c4bdce1b
Rodrigo Centellas <101054195+Rodrigo-Centellas@users.noreply.github.com>
2025-07-25 20:23:09 -0400

    agsaga

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,8 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,13 +20,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        // Vite::prefetch(concurrency: 3);
-                if (config('app.env') === 'production') {
+        // Forzar HTTPS en producción
+        if (config('app.env') === 'production') {
             URL::forceScheme('https');
         }
         
         // Configurar la URL base para subdirectorios
         URL::forceRootUrl(config('app.url'));
-        
     }
 }
